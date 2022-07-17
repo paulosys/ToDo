14d90769de86a2018ce513f60b3a02c8f2da482a
paulosys <paulosilvasergio486@gmail.com>
2022-07-17 02:44:31 -0300

    Desafio 1 - Finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,15 +9,30 @@ export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task
+    const newTask = {
+      id: tasks.length + 1,
+      title: newTaskTitle,
+      done: false,
+    }
+    setTasks([...tasks, newTask])
   }
 
   function handleToggleTaskDone(id: number) {
-    //TODO - toggle task done if exists
+    const updatedTasks = tasks.map(task => ({ ...task }));
+    const taskToToogle = updatedTasks.find(task => task.id === id);
+    
+    if (!taskToToogle)
+      return;
+    
+    
+    taskToToogle.done = !taskToToogle.done;
+    console.log("aaa")
+    setTasks(updatedTasks)
+
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    setTasks(tasks.filter(task => task.id != id))
   }
 
   return (
